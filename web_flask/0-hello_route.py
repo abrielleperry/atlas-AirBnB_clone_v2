a62383d5916f9412a296766c7b4aeaf9e4629e6a
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-07 12:52:26 -0600

    pep8

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,10 +3,12 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_route():
     """ display greeting """
     return "Hello HBNB!"
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port='5000')
