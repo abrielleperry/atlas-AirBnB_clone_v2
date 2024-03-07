72603e3faf6541bc09363adaacabaec7dcb406e7
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-07 12:47:20 -0600

    missing underscore

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" simple single server """
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_route():
+    """ display greeting """
+    return "Hello HBNB!"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port='5000')
