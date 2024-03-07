d93aab7cec4af2c257b2341d85e601e4f14bb87c
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-07 15:39:43 -0600

    take number

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -22,6 +22,17 @@ def text_route(text):
     text = text.replace('_', ' ')
     return f"C {text}"
 
+@app.route('/python/<text>')
+def python_route(text='is cool'):
+    """ handles route /c/<text> returns string replacing _ with spaces """
+    text = text.replace('_', ' ')
+    return "Python {text}"
+
+@app.route('/number/<n>')
+def is_number_route(text, n=int):
+    """ handles route /c/<text> returns string replacing _ with spaces """
+    n = n.replace('_', ' ')
+    return "{n} is a number"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port='5000')
