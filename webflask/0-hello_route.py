253ed1329d73c8deeb461f2dbfa8e8b06ba22013
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-07 12:13:26 -0600

    curling

--- webflask/0-hello_route.py
+++ webflask/0-hello_route.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python
+""" simple single server """
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_route():
+    """ display greeting """
+    return "Hello HBNB!"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port='5000')
