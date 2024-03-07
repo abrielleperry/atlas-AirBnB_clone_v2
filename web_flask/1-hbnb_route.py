103e795c05f7692afe60512fe11700f3c0a09152
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-07 14:56:46 -0600

    forgot to save after rm !

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -12,7 +12,7 @@ def hello_route():
 @app.route('/hbnb')
 def hbnb_route():
     """ display greeting """
-    return "HBNB!"
+    return "HBNB"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port='5000')
