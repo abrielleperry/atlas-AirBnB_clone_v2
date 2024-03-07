874fbe364515dfcf35f177b31ea991648d8110d6
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-07 16:09:21 -0600

    wrong file

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -28,11 +28,5 @@ def python_route(text='is cool'):
     text = text.replace('_', ' ')
     return "Python {text}"
 
-@app.route('/number/<n>')
-def is_number_route(n):
-    """ handles route /c/<text> returns string replacing _ with spaces """
-    if n.isdigit():
-        return f"{n} is a number"
-
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port='5000')
