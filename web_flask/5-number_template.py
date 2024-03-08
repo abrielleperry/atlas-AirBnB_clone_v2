f0724a10ce6b77482b8aa9a3c82d6f3a476f79ce
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-08 16:03:30 -0600

    display value of n

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -39,7 +39,7 @@ def number_route(n):
 @app.route('/number_template/<int:n>')
 def display_number_route(n):
     """ handles route /number/<n>, optional parameter """
-    return render_template('5-number.html')
+    return render_template('5-number.html', n=n)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port='5000')
