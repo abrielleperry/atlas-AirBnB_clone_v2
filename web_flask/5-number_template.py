66c3cb71dd53279b9b895dcaa1a80b01f235c87a
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-08 14:29:44 -0600

    adding 5 files

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" simple single server """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_route():
+    """ display greeting """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb_route():
+    """ handles route /hbnb and returns string """
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def text_route(text):
+    """ handles route /c/<text> returns string replacing _ with spaces """
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def python_route(text='is cool'):
+    """ handles route /python/<text>, optional parameter, defaults is cool """
+    text = text.replace('_', ' ')
+    return f"Python {text}"
+
+
+@app.route('/number/<int:n>')
+def number_route(n):
+    """ handles route /number/<n>, optional parameter """
+    return f"{n} is a number"
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port='5000')
