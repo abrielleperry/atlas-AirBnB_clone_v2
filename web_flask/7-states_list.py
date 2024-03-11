eca14effcf141ac059581ff050433ab5539df332
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-11 15:10:01 -0500

    teardown db

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
 """ simple single server """
 from flask import Flask, render_template, request
+from models import storage
 app = Flask(__name__)
+
+@app.route('/states_list')
+def get_db():
+    """ display greeting """
+    states = sorted(list(storage.all("State").values()))
+    return render_template(7-states_list.html, states=states)
+
+@app.teardown_appcontext
+def close_db():
+    """ teardown database """
+    storage.close()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port='5000')
