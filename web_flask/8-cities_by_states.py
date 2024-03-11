a1bd19759c69c04dff9f0445d93374cbe21c9755
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-11 15:58:30 -0500

    city name

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+""" simple single server """
+from flask import Flask, render_template, request
+from models import storage
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states')
+def cities_by_states():
+    """ display greeting """
+    states = sorted(list(storage.all("State").values()),
+                    key=lambda state: state.name)
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def close_db(exception=None):
+    """ teardown database """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port='5000')
