e156885176f2fcc388cae658d3fbcfde20f7add6
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-11 15:35:56 -0500

    pep8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,16 +4,20 @@ from flask import Flask, render_template, request
 from models import storage
 app = Flask(__name__)
 
+
 @app.route('/states_list')
 def get_db():
     """ display greeting """
-    states = sorted(list(storage.all("State").values()), key=lambda state: state.name)
+    states = sorted(list(storage.all("State").values()),
+                    key=lambda state: state.name)
     return render_template('7-states_list.html', states=states)
+
 
 @app.teardown_appcontext
 def close_db(exception=None):
     """ teardown database """
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port='5000')
