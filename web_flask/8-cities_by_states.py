43ef8e2e3f0e1502205558e9c0627a3f9d97e1a3
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-11 19:28:39 -0500

    7-dump worked

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
 """ simple single server """
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from models import storage
+
 app = Flask(__name__)
 
 
-@app.route('/cities_by_states')
+@app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
     """ display greeting """
     states = sorted(list(storage.all("State").values()),
-                    key=lambda state: state.name)
-    return render_template('7-states_list.html', states=states)
+                key=lambda state: state.name)
+    return render_template('8-cities_by_states.html', states=states)
 
 
 @app.teardown_appcontext
 def close_db(exception=None):
     """ teardown database """
     storage.close()
-
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port='5000')
